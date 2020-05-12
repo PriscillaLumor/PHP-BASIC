9ccbd11eb3cf7bb4ad96ac5bdf8ac64d958df32e
=Priscilla Lumor <=>
2020-05-12 16:40:56 +0200

    taak08 en 09

---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        $voornaam1 = "Priscilla";
+        $voornaam2 = "Phineas";
+        $voornaam3 = "Ferb";
+        echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+    ?>
+
+</body>
+</html>
