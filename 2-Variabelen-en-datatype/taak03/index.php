1fa6cc9bf5039ceac879b17f549e672c2d523301
=Priscilla Lumor <=>
2020-05-12 14:33:12 +0200

    taak03

---
+++ 2-Variabelen-en-datatype/taak03/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        $leeftijd = "24";
+        echo $leeftijd."<br>";
+
+        $melk = "€1.10";
+        echo $melk;
+    ?>  
+
+</body>
+</html>
