b2bdc980f7a015999f4237d83d071c08454b57cd
=Priscilla Lumor <=>
2020-05-13 20:15:51 +0200

    taak02

---
+++ 3-Arrays/taak02/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        $myDogNames = array("Rocky","Bobby","Buddy");
+        $myDogNames[1] = "Twinky";
+        echo $myDogNames[0],$myDogNames[1],$myDogNames[2];
+        
+        //echo $myDogNames[1];
+    ?>
+
+</body>
+</html>
