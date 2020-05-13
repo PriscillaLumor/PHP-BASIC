3ab3f2ec034ee34db17dbc336cf5ed00ed3f5fd4
=Priscilla Lumor <=>
2020-05-13 19:28:03 +0200

    taak01

---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        $myMovies = array("Deep Blue Sea","Blade","Home Alone");
+        echo "<li>$myMovies[0]</li><li>$myMovies[1]</li><li>$myMovies[2]</li>";
+    ?>
+
+</body>
+</html>
