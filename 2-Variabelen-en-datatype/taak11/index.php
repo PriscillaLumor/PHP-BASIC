78e7aeb018862d4f49fdeef26bbbb9a7e614197c
=Priscilla Lumor <=>
2020-05-13 11:19:55 +0200

    taak11

---
+++ 2-Variabelen-en-datatype/taak11/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        $test = "OV";
+        $test1 = 24;
+        $test2 = 0.5;
+
+        var_dump($test,$test1,$test2);
+    ?>
+
+</body>
+</html>
