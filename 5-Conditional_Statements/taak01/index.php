bd184d3f0930d3ba86241de8a4e512ee06ef4196
=Priscilla Lumor <=>
2020-05-14 12:03:57 +0200

    taak01

---
+++ 5-Conditional_Statements/taak01/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+        $test1 = 10;
+        $test2 = "Prettig dag";
+        var_dump($test1 == $test2);
+        var_dump($test1 === $test2);
+        var_dump($test1 != $test2);
+        var_dump($test1 > $test2);
+        var_dump($test1 < $test2);
+        var_dump($test1 <> $test2);
+        var_dump($test1 >= $test2);
+        var_dump($test1 <= $test2);
+        
+    ?>
+
+</body>
+</html>
