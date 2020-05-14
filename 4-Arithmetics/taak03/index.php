5a2b78de8862ceb51f5e263649c10d88cfe06946
=Priscilla Lumor <=>
2020-05-14 11:32:10 +0200

    taak03

---
+++ 4-Arithmetics/taak03/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        $getal1 = 2;
+        echo ++$getal1;
+    ?>
+
+    <?php
+        $getal1 = 10;
+        echo --$getal1;
+    ?>
+
+</body>
+</html>
